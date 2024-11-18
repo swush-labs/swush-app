20eae570fe74799ea47f85e91da0540938fd4762
muddlebee <anweshknayak@gmail.com>
2024-11-18 12:26:32 +0530

    lint fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { ThemeProvider } from 'next-themes'
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
