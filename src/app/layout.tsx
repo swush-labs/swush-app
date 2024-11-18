11438f2dcb3f89b982e6266115d17aef3df3a8ae
muddlebee <anweshknayak@gmail.com>
2024-11-18 12:24:31 +0530

    fix mobile issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,9 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
-        </ThemeProvider>
       </body>
     </html>
   );
