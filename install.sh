7b4f3a55c0430ddcddcc11bfac7ea9eac523a67d
muddlebee <anweshknayak@gmail.com>
2025-03-08 00:33:54 +0530

    fix scripts

---
+++ install.sh
@@ -0,0 +1,10 @@
+# Clean everything
+pnpm clean
+
+# Remove node_modules
+rm -rf node_modules
+rm -rf packages/api/node_modules
+rm -rf apps/web/node_modules
+
+# Fresh install
+pnpm i
--- start.sh
+++ start.sh
@@ -1,1 +1,2 @@
+# Start development
 nohup pnpm dev > output.log 2>&1 &
